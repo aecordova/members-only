051a88b35be6ca9b445eea0ffc4834678c53518b
acordova <ae_cordova@outlook.com>
2020-03-23 16:29:26 -0600

    Added remember_token field and callback for generating token before_create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+
+  before_create :add_remember_token
+
+  has_secure_password
+
+  def add_remember_token
+    self.remember_token = digest(new_token)
+  end
+  
+  # Generate a new url safe token
+  def new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Hash token
+  def digest (token)
+    Digest::SHA1.hexdigest (token)
+  end
+
+
+
+
 end
